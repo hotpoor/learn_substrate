1ef10859f259f8c40395c20119eb9aaa817108e1
hotpoor <liwei.xia0223@gmail.com>
2021-01-11 16:24:01 +0800

    线上标准答案公布

---
+++ substrate_node/第一课作业标准答案/substrate_advanced_course_poe_more.rs
@@ -0,0 +1,236 @@
+// lib.rs
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// A FRAME pallet proof of existence with necessary imports
+
+use frame_support::{
+	decl_module, decl_storage, decl_event, decl_error, dispatch, ensure,
+	traits::{Get},
+};
+use frame_system::{self as system, ensure_signed};
+use sp_std::prelude::*;
+use sp_runtime::traits::StaticLookup;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+/// The pallet's configuration trait.
+pub trait Trait: system::Trait {
+	// Add other types and constants required to configure this pallet.
+
+	/// The overarching event type.
+	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+
+	// 附加题答案
+	type MaxClaimLength: Get<u32>;
+}
+
+// This pallet's storage items.
+decl_storage! {
+	// It is important to update your storage name so that your pallet's
+	// storage items are isolated from other pallets.
+	// ---------------------------------vvvvvvvvvvvvvv
+	trait Store for Module<T: Trait> as TemplateModule {
+		Proofs get(fn proofs): map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
+	}
+}
+
+// The pallet's events
+decl_event!(
+	pub enum Event<T> where AccountId = <T as system::Trait>::AccountId {
+		ClaimCreated(AccountId, Vec<u8>),
+		ClaimRevoked(AccountId, Vec<u8>),
+	}
+);
+
+// The pallet's errors
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+		ProofAlreadyExist,
+		ClaimNotExist,
+		NotClaimOwner,
+		ProofTooLong,
+	}
+}
+
+// The pallet's dispatchable functions.
+decl_module! {
+	/// The module declaration.
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		// Initializing errors
+		// this includes information about your errors in the node's metadata.
+		// it is needed only if you are using errors in your pallet
+		type Error = Error<T>;
+
+		// Initializing events
+		// this is needed only if you are using events in your pallet
+		fn deposit_event() = default;
+
+		#[weight = 0]
+		pub fn create_claim(origin, claim: Vec<u8>) -> dispatch::DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			ensure!(!Proofs::<T>::contains_key(&claim), Error::<T>::ProofAlreadyExist);
+
+			// 第二题答案
+			ensure!(T::MaxClaimLength::get() >= claim.len() as u32, Error::<T>::ProofTooLong);
+
+			Proofs::<T>::insert(&claim, (sender.clone(), system::Module::<T>::block_number()));
+
+			Self::deposit_event(RawEvent::ClaimCreated(sender, claim));
+
+			Ok(())
+		}
+
+		#[weight = 0]
+		pub fn revoke_claim(origin, claim: Vec<u8>) -> dispatch::DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			ensure!(Proofs::<T>::contains_key(&claim), Error::<T>::ClaimNotExist);
+
+			let (owner, _block_number) = Proofs::<T>::get(&claim);
+
+			ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+			Proofs::<T>::remove(&claim);
+
+			Self::deposit_event(RawEvent::ClaimRevoked(sender, claim));
+
+			Ok(())
+		}
+
+		#[weight = 0]
+		pub fn transfer_claim(origin, claim: Vec<u8>, dest: <T::Lookup as StaticLookup>::Source) -> dispatch::DispatchResult {
+			let sender = ensure_signed(origin)?;
+
+			ensure!(Proofs::<T>::contains_key(&claim), Error::<T>::ClaimNotExist);
+
+			let (owner, _block_number) = Proofs::<T>::get(&claim);
+
+			ensure!(owner == sender, Error::<T>::NotClaimOwner);
+
+			let dest = T::Lookup::lookup(dest)?;
+
+			Proofs::<T>::insert(&claim, (dest, system::Module::<T>::block_number()));
+
+			Ok(())
+		}
+	}
+}
+
+
+
+// test.rs
+
+// Tests to be written here
+
+use crate::{Error, mock::*};
+use super::*;
+use frame_support::{assert_ok, assert_noop};
+
+#[test]
+fn create_claim_works() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1];
+        assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
+        assert_eq!(Proofs::<Test>::get(&claim), (1, system::Module::<Test>::block_number()));
+    })
+}
+#[test]
+fn create_claim_failed_when_claim_already_exist() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+        assert_noop!(
+            PoeModule::create_claim(Origin::signed(1), claim.clone()),
+            Error::<Test>::ProofAlreadyExist
+        );
+    })
+}
+
+#[test]
+fn create_claim_failed_when_claim_is_too_long() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1, 2, 3, 4, 5, 6];
+        
+        assert_noop!(
+            PoeModule::create_claim(Origin::signed(1), claim.clone()),
+            Error::<Test>::ProofTooLong
+        );
+    })
+}
+
+#[test]
+fn revoke_claim_works() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+        assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim.clone()));
+    })
+}
+
+#[test]
+fn revoke_claim_failed_when_claim_is_not_exist() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1];
+
+        assert_noop!(
+            PoeModule::revoke_claim(Origin::signed(1), claim.clone()),
+            Error::<Test>::ClaimNotExist
+        );
+    })
+}
+
+#[test]
+fn revoke_claim_failed_with_wrong_owner() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+        assert_noop!(
+            PoeModule::revoke_claim(Origin::signed(2), claim.clone()),
+            Error::<Test>::NotClaimOwner
+        );
+    })
+}
+
+#[test]
+fn transfer_claim_works() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+        assert_ok!(PoeModule::transfer_claim(Origin::signed(1), claim.clone(), 2));
+        assert_eq!(Proofs::<Test>::get(&claim), (2, system::Module::<Test>::block_number()));
+    })
+}
+
+#[test]
+fn transfer_claim_failed_when_claim_is_not_exist() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1];
+
+        assert_noop!(
+            PoeModule::transfer_claim(Origin::signed(1), claim.clone(), 2),
+            Error::<Test>::ClaimNotExist
+        );
+    })
+}
+
+#[test]
+fn transfer_claim_failed_with_wrong_owner() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![0, 1];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+
+        assert_noop!(
+            PoeModule::transfer_claim(Origin::signed(2), claim.clone(), 3),
+            Error::<Test>::NotClaimOwner
+        );
+    })
+}
